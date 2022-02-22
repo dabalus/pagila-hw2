90fea27107111caa86b0f0401dde51a1b0e7d8ca
Your Name <you@example.com>
2022-02-21 20:46:36 -0800

    find new homework

--- sql/11.sql
+++ sql/11.sql
@@ -1,4 +1,1 @@
-/*
- * List the name of all actors who have appeared in a movie that has the 'Behind the Scenes' special_feature
- */
 select distinct concat(first_name,' ', last_name) "Actor Name"  from (select film_id, title, unnest(special_features) sf from film) T  JOIN film_actor on film_actor.film_id = T.film_id JOIN actor on actor.actor_id = film_actor.actor_id where sf = 'Behind the Scenes';
